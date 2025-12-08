fd6602a056e14a89bb64eb2b3e2e9e94ba2513f0
juusokasperi <juusor@gmail.com>
2025-12-08 20:51:10 +0200

    Redo of main.c

--- srcs/main.c
+++ srcs/main.c
@@ -1,67 +1,122 @@
+#define MEMARENA_IMPLEMENTATION
 #include "memarena.h"
 #include "ast.h"
-#include "lexer.h"
 #include "ir.h"
 #include "jit.h"
-#include "shared_types.h"
-#include "parser.h"
+#include "compile.h"
 #include "utils.h"
 #include <stdio.h>
-#include <errno.h>
+#include <sys/mman.h>
 
-static int handle_cleanup(Arena *ast_arena, Arena *jit_arena, FileMap *file)
+static void cleanup_files(CompilationContext *ctx)
 {
-		arena_free(ast_arena);
-		arena_free(jit_arena);
-		munmap((void *)file->data, file->length);
-		return (1);
+	for (size_t i = 0; i < ctx->count; ++i)
+	{
+		if (ctx->units[i].file.data)
+			munmap((void *)ctx->units[i].file.data, ctx->units[i].file.length);
+	}
 }
 
-// Messy main just used for testing
 int main(int argc, char **argv)
 {
-	(void) argc; // void for now
-				 // also for future argv++ to skip program name and start from the file(s)
-	argv[0] = "test.txt";
-
-	FileMap file;
-	if (!parse_input(argv, &file))
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <file1.c> [file2.c ...]\n", argv[0]);
 		return (1);
-	
-	print_file(&file);
+	}
 
 	Arena ast_arena = arena_init(PROT_READ | PROT_WRITE);
 	Arena jit_arena = arena_init(PROT_READ | PROT_WRITE);
 
-	Lexer lexer;
-    lexer_init(&lexer, &file);
-	
-	ASTNode *root = parse(&lexer, &ast_arena);
-	if (!root)
-		return handle_cleanup(&ast_arena, &jit_arena, &file);
-	
+	CompilationContext ctx;
+	compile_ctx_init(&ctx, &ast_arena, argc - 1);
+
+	for (int i = 1; i < argc; ++i)
+	{
+		if (!compile_ctx_add_file(&ctx, argv[i]))
+		{
+			cleanup_files(&ctx);
+			arena_free(&ast_arena);
+			arena_free(&jit_arena);
+			return (1);
+		}
+	}
+
+	if (!compile_parse_all(&ctx))
+	{
+		fprintf(stderr, "\nParsing failed.\n");
+		compile_print_errors(&ctx);
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
+	}
+
+	if (!compile_analyze_all(&ctx))
+	{
+		fprintf(stderr, "\nSemantic analysis failed.\n");
+		compile_print_errors(&ctx);
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
+	}
+
+	printf("\nCompilation successful!\n");
+
+	ASTNode *entry = compile_get_entry_point(&ctx);
+	if (!entry)
+	{
+		fprintf(stderr, "No entry point found (need a 'main' function)\n");
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
+	}
+
 	printf("\n--- AST Structure ---\n");
-    print_ast(root, 0);
+	print_ast(entry, 0);
 	printf("\n");
 
-	IRFunction *ir = ir_gen(&jit_arena, root);
+	IRFunction *ir = ir_gen(&jit_arena, entry);
+	if (!ir)
+	{
+		fprintf(stderr, "IR generation failed.\n");
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
+	}
 	ir_print(ir);
 
 	JITResult jit = jit_compile(&jit_arena, ir);
-	if (jit.code)
+	if (!jit.code)
 	{
-		printf("Generated %zu bytes of x86-64 machine code.\n", jit.size);
-		if (!arena_set_prot(&jit_arena, PROT_READ | PROT_EXEC))
-		{
-			perror("Failed to set executable permissions");
-			return handle_cleanup(&ast_arena, &jit_arena, &file);
-		}
-		printf("\n--- EXECUTION ---\n");
-		JITFunc func = (JITFunc)jit.code;
-		int64_t result = func();
-		printf("Result: %lld\n", result);
+		fprintf(stderr, "JIT compilation failed.\n");
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
 	}
 
-	handle_cleanup(&ast_arena, &jit_arena, &file);
+	printf("Generated %zu bytes of x86-64 machine code.\n", jit.size);
+	if (!arena_set_prot(&jit_arena, PROT_READ | PROT_EXEC))
+	{
+		perror("Failed to set executable permissions");
+		cleanup_files(&ctx);
+		arena_free(&ast_arena);
+		arena_free(&jit_arena);
+		return (1);
+	}
+
+	printf("\n--- Execution ---\n");
+	JITFunc func = (JITFunc)jit.code;
+	int64_t result = func();
+	printf("Result: %lld\n", result);
+
+	cleanup_files(&ctx);
+	arena_free(&ast_arena);
+	arena_free(&jit_arena);
+
 	return (0);
 }
