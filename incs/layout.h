e6add71703569eab772c9467a9c0356870c41446
juusokasperi <juusor@gmail.com>
2025-12-10 20:56:39 +0200

    Added some colors and styling

---
+++ incs/layout.h
@@ -0,0 +1,22 @@
+#ifndef LAYOUT_H
+# define LAYOUT_H
+
+#define RESET       "\033[0m"
+#define BOLD        "\033[1m"
+#define DIM         "\033[2m"
+#define GREEN       "\033[0;32m"
+#define BOLD_GREEN  "\033[1;32m"
+#define CYAN        "\033[0;36m"
+#define WHITE       "\033[0;97m"
+#define BOLD_WHITE  "\033[1;97m"
+#define RED			"\033[0;31m"
+#define BOLD_RED	"\033[1;31m"
+
+#define BOX_TL "╔" // Top Left
+#define BOX_TR "╗" // Top Right
+#define BOX_BL "╚" // Bottom Left
+#define BOX_BR "╝" // Bottom Right
+#define BOX_H  "═" // Horizontal
+#define BOX_V  "║" // Vertical
+
+#endif
