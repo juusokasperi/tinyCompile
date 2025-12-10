a7acb4a9d287af50243619c1ea9b2f64910316c6
juusokasperi <juusor@gmail.com>
2025-12-10 21:22:39 +0200

    Styling functions

--- incs/utils.h
+++ incs/utils.h
@@ -11,11 +11,14 @@
 # include <stdlib.h>
 # include "shared_types.h"
 # include "ast.h"
+# include "layout.h"
 
 int safe_open(char *file);
 FileMap map_input(int fd);
 bool parse_input(char **argv, FileMap *out_file);
 void print_ast(ASTNode *node, int indent);
 void print_file(FileMap *file);
+void print_header(void);
+void print_phase(int step, const char *name);
 
 #endif
