2d1329c4b2d3c7e515f62e5a655e7bf4ee15bc9d
juusokasperi <juusor@gmail.com>
2025-12-23 19:39:29 +0200

    More tests

---
+++ tests/fail/03_redeclaration.c
@@ -0,0 +1,5 @@
+int main() {
+    int x = 5;
+    int x = 10;  // ERROR: redeclaration
+    return x;
+}
---
+++ tests/fail/04_conflicting_types.c
@@ -0,0 +1,6 @@
+int foo(int x);
+int foo(int x, int y);  // ERROR: conflicting declarations
+
+int main() {
+    return 0;
+}
